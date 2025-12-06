8b8f0460bce9ecfe536a93ec590dae44670c620c
Vlad <192461579+Vlad-XD@users.noreply.github.com>
2025-12-06 03:37:14 -0800

    Create main files for website

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Resume from "./components/Resume.jsx";
+import "./css/normalize.css";
+import "./css/reset.css";
+import "./css/main.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Resume />
+  </StrictMode>,
+);
